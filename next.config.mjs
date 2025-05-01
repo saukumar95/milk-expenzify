e512d54d04cee9b17090c278829f87dd544f50bd
Saurabh Kumar <saukumar95@gmail.com>
2025-05-01 22:52:42 +0530

    fix: update basePath in next.config.mjs for consistency across environments

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,7 @@ const nextConfig = {
   },
   reactStrictMode: true,
   devIndicators: false,
-  basePath: '/milk-expenzify',
+  basePath: '/',
   experimental: {
     serverActions: {
       allowedOrigins: ['*'],
